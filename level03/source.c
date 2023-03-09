11cbd7ec80031f4d0a74e84cff0fadeb2b890f09
Bumblebee Goat <bgoat@pr-f3.kzn.21-school.ru>
2023-03-09 22:47:36 +0300

    source.c, walktrhough need to be finised, flag is found

---
+++ level03/source.c
@@ -0,0 +1,44 @@
+#include <time.h>
+#include <stdio.h>
+
+void decrypt(int pass) {
+    char str[17] = "Q}|u`sfg~sf{}|a3";
+
+    while () ??? работа со строкой
+
+    if (strncmp(str, "Congratulations!", 17) == 0)
+        system("/bin/sh");
+    else
+        puts("\nInvalid Password");
+
+
+}
+
+void test(int arg1, int arg2) {
+    int var = arg2 - arg1;
+
+    if (21 < var )
+        decrypt(var);
+    rand(arg2);
+    decrypt(rand());
+    return;
+}
+
+int main(void) {
+
+    time_t seconds;
+
+    seconds = time(NULL);
+
+    srand(0);
+    puts("***********************************");
+    puts("*\t\tlevel03\t\t**");
+    puts("***********************************");
+    puts("Password:");
+
+    int pass = scanf("%d");
+    
+    test(pass, 322424845);
+    return 0;
+
+}
