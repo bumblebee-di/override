25db823caa15c9d4adb391c3435e52f6eb325e49
Crabb Marsha <cmarsha@ge-i1.kzn.21-school.ru>
2023-03-16 23:36:22 +0300

    string logic and python script

--- level03/source.c
+++ level03/source.c
@@ -1,42 +1,45 @@
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
 
 void decrypt(int pass) {
     char str[17] = "Q}|u`sfg~sf{}|a3";
 
-    while () ??? работа со строкой
+    for (int i = 0; i < 16; i++) {
+        str[i] = str[i] ^ pass;
+    }
 
     if (strncmp(str, "Congratulations!", 17) == 0)
-        system("/bin/sh");
+        puts("Congratulations!\n");
+        // system("/bin/sh");
     else
         puts("\nInvalid Password");
-
-
 }
 
 void test(int arg1, int arg2) {
     int var = arg2 - arg1;
 
-    if (21 < var )
+    if (21 > var )
         decrypt(var);
-    rand(arg2);
-    decrypt(rand());
+    // rand(arg2);
+    // decrypt(rand());
     return;
 }
 
 int main(void) {
 
-    time_t seconds;
+    // time_t seconds;
 
-    seconds = time(NULL);
+    // seconds = time(NULL);
 
-    srand(0);
+    // srand(0);
     puts("***********************************");
     puts("*\t\tlevel03\t\t**");
     puts("***********************************");
     puts("Password:");
 
-    int pass = scanf("%d");
+    int pass = 0;
+    scanf("%d", &pass);
     
     test(pass, 322424845);
     return 0;
